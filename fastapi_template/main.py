32cfb21295bb8f91a744b02effdd19eae110eecb
Ethan Harstad <eharstad@athene.com>
2021-09-19 22:00:14 -0500

    Add books endpoints

--- fastapi_template/main.py
+++ fastapi_template/main.py
@@ -11,7 +11,19 @@ class Author(BaseModel):
     name: str
 
 
-AUTHORS = {1: Author(id=1, name="John Doe")}
+class Book(BaseModel):
+    isbn: int
+    title: str
+    author_id: int
+
+
+AUTHORS = {
+    1: Author(id=1, name="John Doe"),
+}
+
+BOOKS = {
+    123456789012: Book(isbn=123456789012, title="Foo Bar", author_id=1),
+}
 
 
 @app.get("/")
@@ -21,18 +33,34 @@ async def read_root():
 
 @app.get("/authors", response_model=List[Author])
 async def list_authors():
-    return AUTHORS
-
-
-@app.get("/authors/{author_id}", response_model=Author)
-async def read_author(author_id: int):
-    return AUTHORS[author_id]
+    return list(AUTHORS.values())
 
 
 @app.post("/authors", response_model=Author)
 async def create_author(author: Author):
     AUTHORS[author.id] = author
     return author
+
+
+@app.get("/authors/{author_id}", response_model=Author)
+async def read_author(author_id: int):
+    return list(AUTHORS[author_id])
+
+
+@app.get("/books", response_model=List[Book])
+async def list_books():
+    return list(BOOKS.values())
+
+
+@app.post("/books", response_model=Book)
+async def create_book(book: Book):
+    BOOKS[book.isbn] = book
+    return book
+
+
+@app.get("/books/{isbn}", response_model=Book)
+async def read_book(isbn: int):
+    return BOOKS[isbn]
 
 
 def start():
