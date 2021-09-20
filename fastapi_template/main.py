1314a967b36a7a1367361bb256d8813fc32a3602
Ethan Harstad <eharstad@athene.com>
2021-09-20 01:52:58 -0500

    Add proper relationships to book/author

--- fastapi_template/main.py
+++ fastapi_template/main.py
@@ -3,7 +3,7 @@ from sqlalchemy.engine import result
 import uvicorn
 from fastapi import Depends, FastAPI, HTTPException
 from pydantic import BaseModel
-from sqlmodel import Field, Session, SQLModel, create_engine, select
+from sqlmodel import Field, Relationship, Session, SQLModel, create_engine, select
 
 app = FastAPI()
 
@@ -13,6 +13,7 @@ class AuthorBase(SQLModel):
 class Author(AuthorBase, table=True):
     __tablename__ = "authors"
     id: Optional[int] = Field(default=None, primary_key=True)
+    books: List["Book"] = Relationship(back_populates="author")
 
 class AuthorCreate(AuthorBase):
     pass
@@ -20,12 +21,26 @@ class AuthorCreate(AuthorBase):
 class AuthorRead(AuthorBase):
     id: int
 
-class Book(SQLModel, table=True):
-    __tablename__ = "books"
+class AuthorReadWithBooks(AuthorBase):
+    id: int
+    books: List["BookBase"]
+
+class BookBase(SQLModel):
     isbn: int = Field(primary_key=True)
     title: str
-    author_id: int
 
+class Book(BookBase, table=True):
+    __tablename__ = "books"
+    author_id: int = Field(foreign_key="authors.id")
+    author: Author = Relationship(back_populates="books")
+
+class BookCreate(BookBase):
+    author_id: int = Field(foreign_key="authors.id")
+
+class BookRead(BookBase):
+    author: AuthorRead
+
+AuthorReadWithBooks.update_forward_refs()
 
 sqlite_file_name = (
     "/Users/e72816/workspace/AtheneUSA/templates/fastapi-template/database.db"
@@ -60,11 +75,12 @@ async def create_author(author: AuthorCreate, session: Session = Depends(get_db_
     return db_author
 
 
-@app.get("/authors/{author_id}", response_model=AuthorRead)
+@app.get("/authors/{author_id}", response_model=AuthorReadWithBooks)
 async def read_author(author_id: int, session: Session = Depends(get_db_session)):
     author = session.get(Author, author_id)
     if not author:
         raise HTTPException(status_code=404, detail="Author not found")
+    print(author.books)
     return author
 
 
@@ -78,21 +94,22 @@ async def delete_author(author_id: int, session: Session = Depends(get_db_sessio
     return {"ok": True}
 
 
-@app.get("/books", response_model=List[Book])
+@app.get("/books", response_model=List[BookRead])
 async def list_books(session: Session = Depends(get_db_session)):
     books = session.exec(select(Book)).all()
     return books
 
 
-@app.post("/books", response_model=Book)
-async def create_book(book: Book, session: Session = Depends(get_db_session)):
-    session.add(book)
+@app.post("/books", response_model=BookRead)
+async def create_book(book: BookCreate, session: Session = Depends(get_db_session)):
+    db_book = Book.from_orm(book)
+    session.add(db_book)
     session.commit()
-    session.refresh(book)
-    return book
+    session.refresh(db_book)
+    return db_book
 
 
-@app.get("/books/{isbn}", response_model=Book)
+@app.get("/books/{isbn}", response_model=BookRead)
 async def read_book(isbn: int, session: Session = Depends(get_db_session)):
     book = session.get(Book, isbn)
     if not book:
