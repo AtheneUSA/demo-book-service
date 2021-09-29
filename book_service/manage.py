d1bc3ee63c7d76d06e50e0787d614d3af9d96bfe
Ethan Harstad <eharstad@athene.com>
2021-09-29 09:27:10 -0500

    Formatting

--- book_service/manage.py
+++ book_service/manage.py
@@ -5,24 +5,25 @@ import typer
 
 from book_service.main import AuthorCreate, AuthorRead, BookCreate, BookRead
 
-host = 'http://localhost:8000'
+host = "http://localhost:8000"
 
 app = typer.Typer()
 fake = Faker()
 random.seed()
 
+
 def _make_author() -> AuthorCreate:
-    return AuthorCreate(
-        name=fake.name()
-    )
+    return AuthorCreate(name=fake.name())
+
 
 def _make_book(author_id: int) -> BookCreate:
     book = BookCreate(
-        isbn=int(fake.isbn13(separator='')),
+        isbn=int(fake.isbn13(separator="")),
         title=fake.sentence(nb_words=4),
         author_id=author_id,
     )
     return book
+
 
 def _post_author(author: AuthorCreate) -> AuthorRead:
     post_data = author.json()
@@ -31,12 +32,14 @@ def _post_author(author: AuthorCreate) -> AuthorRead:
     data = r.json()
     return AuthorRead(**data)
 
+
 def _post_book(book: BookCreate) -> BookRead:
     post_data = book.json()
     r = requests.post(f"{host}/books", data=post_data)
     r.raise_for_status()
     data = r.json()
     return BookRead(**data)
+
 
 @app.command()
 def generate_books(n: int = 1):
@@ -51,12 +54,14 @@ def generate_books(n: int = 1):
         data = _post_book(book)
         typer.echo(f"{book.title} by {author.name}")
 
+
 @app.command()
 def generate_authors(n: int = 1):
     for _ in range(n):
         author = _make_author()
         data = _post_author(author)
         typer.echo(data)
+
 
 if __name__ == "__main__":
     app()
