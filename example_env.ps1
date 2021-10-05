3176f59d32d9b4adad30d7b13e9e1dc2b370283c
Doug Renze <drenze@athene.com>
2021-10-05 07:38:18 -0500

    add sample environment variables for Windows deploy

--- example_env.ps1
+++ example_env.ps1
@@ -12,5 +12,9 @@ $env:ELASTIC_APM_ENVIRONMENT = "demo"  # Usually one of: (dev|qa|stage|prod)
 $env:ELASTIC_APM_LOG_LEVEL = "info"  # One of: (off|critical|error|warning|info|debug|trace)
 $env:ELASTIC_APM_RECORDING = "true"  # (true|false)
 $env:ELASTIC_APM_SERVER_URL = "https://elk.onintranet.com:8200"
+<<<<<<< HEAD
 $env:ELASTIC_APM_SERVICE_NAME = "book-service-$($($(whoami) -Split '\\')[-1])"
+=======
+$env:ELASTIC_APM_SERVICE_NAME = "demo-book-service-$($($(whoami) -Split '\\')[-1])"
+>>>>>>> 8720c01 (add sample environment variables for Windows deploy)
 $env:ELASTIC_APM_VERIFY_SERVER_CERT = "False"  # (True|False)
